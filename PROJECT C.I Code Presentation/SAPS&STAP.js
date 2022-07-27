bf18d5009f79040fadc40e294e2ab369af7a9394
Xinyuanso <51281832+Xinyuanso@users.noreply.github.com>
2022-07-27 16:38:12 +0800

    Add files via upload

---
+++ PROJECT C.I Code Presentation/SAPS&STAP.js
@@ -0,0 +1,13 @@
+function eventsbtn()
+{
+    window.location="Events.html";
+}
+function club_membershipbtn() 
+{
+    window.location="ClubMembership.html";
+}
+
+function verify_approve_stapbtn()
+{
+    window.location="VerifyApproveSTAP.html";
+}
