c296915bc46f8bb50b4db3ea7dc2fa85a4930e37
Xinyuanso <51281832+Xinyuanso@users.noreply.github.com>
2022-07-27 16:59:31 +0800

    Add files via upload

---
+++ BaseNavigation.js
@@ -0,0 +1,38 @@
+function SelfServicebtn()
+{
+        window.location="SelfServiceMain.html";   
+}
+
+function e_votingbtn()
+{ 
+        window.location="E-voting.html";
+}
+
+function campusbtn()
+{ 
+        window.location="CampusCom.html";
+}
+function recordsbtn()
+{ 
+        window.location="RecordsAndEnrollment.html";
+}
+function SAPSbtn()
+{ 
+        window.location="SAPS&STAPManagement.html";
+}
+function setupbtn()
+{ 
+        window.location="SetUpSACR.html";
+}
+function venuebtn()
+{
+        window.location="VenueReservationApplication.html";
+}
+function votbtn()
+{
+        window.location="EVS.html";
+}
+function biobtn()
+{
+        window.location="Guardian.html";
+}
